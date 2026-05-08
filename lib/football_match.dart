01d9628a6b0f78dad174bf7c8a9685bec46a6c4c
sinthia-03 <sinthiaorita37@gmail.com>
2026-05-08 12:32:21 +0600

    cloud firestore DB

---
+++ lib/football_match.dart
@@ -0,0 +1,42 @@
+class FootballMatch {
+  final String id;
+  final String team1Name;
+  final String team2Name;
+  final String winnerTeam;
+  final int team1Score;
+  final int team2Score;
+  final bool isRunning;
+
+  FootballMatch({
+    required this.id,
+    required this.team1Name,
+    required this.team2Name,
+    required this.winnerTeam,
+    required this.team1Score,
+    required this.team2Score,
+    required this.isRunning,
+  });
+
+  factory FootballMatch.fromJson(String id, Map<String, dynamic> json) {
+    return FootballMatch(
+      id: id,
+      team1Name: json['team1_name'],
+      team2Name: json['team2_name'],
+      winnerTeam: json['winner_team'],
+      team1Score: json['team1_score'],
+      team2Score: json['team2_score'],
+      isRunning: json['isRunning'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'team1_name': team1Name,
+      'team2_name': team2Name,
+      'winner_team': winnerTeam,
+      'team1_score': team1Score,
+      'team2_score': team2Score,
+      'isRunning': isRunning,
+    };
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:live_score_app/football_match.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,20 +12,161 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String _footballCollectionName = 'football';
+
+  // bool _getMatchesInProgress = false;
+  // List<FootballScore> _footballMatchesList = [];
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getFootballMatches();
+  // }
+  //
+  // // One time get operation
+  // Future<void> _getFootballMatches() async {
+  //   _footballMatchesList.clear();
+  //   _getMatchesInProgress = true;
+  //   setState(() {});
+  //
+  //   QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
+  //       .instance
+  //       .collection('football')
+  //       .get();
+  //   for (QueryDocumentSnapshot doc in snapshot.docs) {
+  //     _footballMatchesList.add(
+  //       FootballScore.fromJson(doc.id, doc.data() as Map<String, dynamic>),
+  //     );
+  //   }
+  //   _getMatchesInProgress = false;
+  //   setState(() {});
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
         actions: [
-          IconButton(onPressed: _onTapLogOut,
-              icon: Icon(Icons.logout))
+          IconButton(onPressed: _onTapLogout, icon: Icon(Icons.logout)),
         ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection(_footballCollectionName)
+            .snapshots(),
+        builder:
+            (
+            context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> asyncSnapshot,
+            ) {
+          if (asyncSnapshot.connectionState == .waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (asyncSnapshot.hasError) {
+            return Center(child: Text(asyncSnapshot.error.toString()));
+          }
+
+          if (asyncSnapshot.hasData == false) {
+            return Center(child: Text('No data available'));
+          }
+
+          List<FootballMatch> footballMatchesList = [];
+          for (QueryDocumentSnapshot doc in asyncSnapshot.data!.docs) {
+            footballMatchesList.add(
+              FootballMatch.fromJson(
+                doc.id,
+                doc.data() as Map<String, dynamic>,
+              ),
+            );
+          }
+
+          return ListView.separated(
+            itemCount: footballMatchesList.length,
+            itemBuilder: (context, index) {
+              final FootballMatch match = footballMatchesList[index];
+
+              return Dismissible(
+                key: Key(match.id),
+                onDismissed: (_) {
+                  _deleteMatchItem(match.id);
+                },
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: match.isRunning
+                        ? Colors.green
+                        : Colors.grey,
+                    radius: 8,
+                  ),
+                  title: Text('${match.team1Name} vs ${match.team2Name}'),
+                  subtitle: Text('Winner team: ${match.winnerTeam}'),
+                  trailing: Text(
+                    '${match.team1Score} - ${match.team2Score}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (_, _) => Divider(height: 8),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapAddButton,
+        child: Icon(Icons.add),
       ),
     );
   }
 
-  void _onTapLogOut(){
+  void _onTapAddButton() {
+    try {
+      FootballMatch newFootballMatch = FootballMatch(
+          id: 'argvsuru',
+          team1Name: 'Argentina',
+          team2Name: 'Uruguay',
+          winnerTeam: 'Argentina',
+          team1Score: 1,
+          team2Score: 1,
+          isRunning: false
+      );
+
+      // Creates new item every time
+      // FirebaseFirestore.instance
+      //     .collection(_footballCollectionName)
+      //     .add(newFootballMatch.toJson());
+      // FirebaseFirestore.instance
+      //     .collection(_footballCollectionName)
+      //     .doc(newFootballMatch.id) // Because of this id
+      //     .update(newFootballMatch.toJson());
+      FirebaseFirestore.instance
+          .collection(_footballCollectionName)
+          .doc(newFootballMatch.id) // Because of this id
+          .set(newFootballMatch.toJson());
+    } on Exception catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString()))
+      );
+    }
+  }
+
+  Future<void> _deleteMatchItem(String docId) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection(_footballCollectionName)
+          .doc(docId)
+          .delete();
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Match deleted!'))
+      );
+    } on Exception catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString()))
+      );
+    }
+  }
+
+  void _onTapLogout() {
     FirebaseAuth.instance.signOut();
   }
 }
