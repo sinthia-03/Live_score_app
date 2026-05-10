a786dd233ae9ad84fe6708f35e35c587b07d0b51
sinthia-03 <sinthiaorita37@gmail.com>
2026-05-10 23:52:08 +0600

    fore/back/pushnotification/globally

---
+++ lib/fcm_utilis.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:live_score_app/main.dart';
+
+class FcmUtilis {
+  static Future<void> initialize() async {
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      criticalAlert: true,
+      sound: true,
+    );
+
+    // Foreground - App jokhon cholche
+    FirebaseMessaging.onMessage.listen(_handleforegroundNotification);
+
+    // Background/Minimize - Notification e click korle app e dhukbe
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleBackgroundNotification);
+
+    // Killed/Dead State - Background message handler
+    FirebaseMessaging.onBackgroundMessage(handleBackgriundNotification);
+  }
+
+  static void _handleBackgroundNotification(RemoteMessage message) {
+    print(message.data);
+    print(message.notification?.title);
+    print(message.notification?.body);
+  }
+
+  static void _handleforegroundNotification(RemoteMessage message) {
+    print(message.data);
+    print(message.notification?.title);
+    print(message.notification?.body);
+
+    // Navigatorkey check kora dorkar jeno context null na hoy
+    if (MyApp.navigatorkey.currentState?.context != null) {
+      showDialog(
+        context: MyApp.navigatorkey.currentState!.context,
+        builder: (ctx) {
+          return Dialog(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // Column ke choto rakhar jonno
+                children: [
+                  Text(
+                    message.notification?.title ?? '',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(message.notification?.body ?? ''),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
+  }
+}
+
+// Eta class er bairei thakte hobe (Top-level function)
+Future<void> handleBackgriundNotification(RemoteMessage message) async {
+  print("Background message received: ${message.notification?.title}");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:live_score_app/fcm_utilis.dart';
 import 'package:live_score_app/firebase_options.dart';
 import 'package:live_score_app/home_screen.dart';
 import 'package:live_score_app/sign_up_screen.dart';
@@ -24,15 +25,20 @@ Future<void> main() async{
     return true;
   };
 
+  FcmUtilis.initialize();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static GlobalKey<NavigatorState> navigatorkey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorkey,
       home: StreamBuilder( //stream listen
         stream: FirebaseAuth.instance.authStateChanges(), //app start
         builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
